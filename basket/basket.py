1586b58b00563641aaeb8d3b7939a85201fa7e30
nitesh585 <niteshyadav585@gmail.com>
2021-11-16 12:33:02 +0530

    add price and qty to session data

--- basket/basket.py
+++ basket/basket.py
@@ -4,9 +4,22 @@ class Basket:
     can be inherited or overrided, as necessary
     """
 
-    def __init__(self, request) -> None:
+    def __init__(self, request):
         self.session = request.session
         basket = self.session.get("skey")
-        if "skey" not in self.session:
-            basket = self.session["skey"] = {'number':4518}
+        if "skey" not in request.session:
+            basket = self.session["skey"] = {}
         self.basket = basket
+
+    def add(self, product, product_qty):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = str(product.id)
+
+        if product_id not in self.basket:
+            self.basket[product_id] = {
+                "price": str(product.price),
+                "qty": int(product_qty),
+            }
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+from store.models import Product
+from basket.basket import Basket
 
 
 def basket_summary(request):
     return render(request, "store/basket/summary.html")
+
+
+def basket_add(request):
+    basket = Basket(request)
+
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
+        product_qty = int(request.POST.get("productqty"))
+
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, product_qty=product_qty)
+        response = JsonResponse({"test": "data"})
+        return response
