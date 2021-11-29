b1a93dbdad8883ba1aaa8016f207d697012d2136
nitesh585 <niteshyadav585@gmail.com>
2021-11-29 22:34:45 +0530

    registration of user with token

--- account/views.py
+++ account/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.sites.shortcuts import get_current_site
+from .forms import RegistrationForm
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from .token import account_activation_token
 
-# Create your views here.
+
+def account_register(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+
+    if request.method == "POST":
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            user.email = registerForm.cleaned_data["email"]
+            user.set_password(registerForm.cleaned_data["password"])
+            user.is_active = False
+            user.save()
+            # setup email
+            current_site = get_current_site(request)
+            subject = "Activate your Account"
+            message = render_to_string(
+                "account/registration/account_activation_email.html",
+                {
+                    "user": user,
+                    "domain": current_site.domain,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": account_activation_token.make_token(user),
+                },
+            )
+            user.email_user(subject=subject, message=message)
+    else:
+        registerForm = RegistrationForm()
+    return render(request, 'account/registration/register.html', {'form': registerForm})
