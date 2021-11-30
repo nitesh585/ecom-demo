c8c76375e731977c84cebe3bb621bc21bb5cb061
nitesh585 <niteshyadav585@gmail.com>
2021-11-30 23:48:56 +0530

    add urls and templates

---
+++ account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = "account"
+
+urlpatterns = [
+    path("register/", views.account_register, name="register"),
+    path(
+        "activate/<slug:uidb64>/<slug:token>)/", views.account_activate, name="activate"
+    ),
+    path("dashboard/", views.dashboard, name="dashboard"),
+]
