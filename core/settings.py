5c8e2fcc56a51bfdd5bdf0584ddff840cebc58cc
nitesh585 <niteshyadav585@gmail.com>
2021-11-30 23:49:27 +0530

    update setting for sending email

--- core/settings.py
+++ core/settings.py
@@ -138,3 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 AUTH_USER_MODEL = "account.UserBase"
 LOGIN_REDIRECT_URL = "/account/dashboard"
 LOGIN_URL = "/account/login/"
+
+
+# Email setting
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
