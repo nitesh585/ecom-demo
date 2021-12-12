3cf52e5f0f02fab359c8054e99094701ab590a66
nitesh585 <niteshyadav585@gmail.com>
2021-12-12 17:17:50 +0530

    add payment app

--- payment/views.py
+++ payment/views.py
@@ -7,14 +7,17 @@ from basket.basket import Basket
 @login_required
 def BasketView(request):
     basket = Basket(request)
+
     total_price = str(basket.get_total_price())
     total_price = total_price.replace(".", " ")
     total_price = int(total_price)
-    total_price = 10
+    total_price = 100
     print(total_price)
     stripe.api_key = "sk_test_51K5kkkSJJe1bpxbm2639DnadJVGm4fbnov7cPqDle873lNeIkGMsB9Q6JlfTSli0NgJOWZx1J0SeDIB6yn7juLng00ZdUNXnoa"
     intent = stripe.PaymentIntent.create(
-        amount=total_price, currency="gbp", metadata={"userid": request.user.id}
+        amount=total_price,
+        currency="INR",
+        metadata={"userid": request.user.id},
     )
 
     return render(request, "payment/home.html", {"client_secret": intent.client_secret})
